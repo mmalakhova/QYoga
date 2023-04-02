e02322912c2bf5b52354b5a37db5184bba8ae2b6
Mariya Malakhova <malakhova@siberian.pro>
2023-04-03 03:04:00 +0700

    fix: detekt

--- src/test/kotlin/nsu/fit/qyoga/cases/core/exercises/ui/ExerciseViewTest.kt
+++ src/test/kotlin/nsu/fit/qyoga/cases/core/exercises/ui/ExerciseViewTest.kt
@@ -5,6 +5,8 @@ import io.restassured.http.Cookie
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
+import nsu.fit.qyoga.core.exercises.api.dtos.CreateExerciseDto
+import nsu.fit.qyoga.core.exercises.api.model.ExerciseType
 import nsu.fit.qyoga.infra.QYogaAppTestBase
 import nsu.fit.qyoga.infra.db.DbInitializer
 import org.jsoup.Jsoup
@@ -98,6 +100,33 @@ class ExerciseViewTest : QYogaAppTestBase() {
             Assertions.assertThatSpec(body) {
                 node("#createExerciseForm") { exists() }
                 node("#buttons") { exists() }
+            }
+        }
+    }
+
+    @Test
+    fun `QYoga returns exercise search page after creating a new exercise`() {
+        val createDto = CreateExerciseDto(
+            title = "Разминка для спины",
+            description = "",
+            indications = "",
+            contradiction = "",
+            duration = "00:10:00",
+            exerciseType = ExerciseType.WarmUp
+        )
+
+        Given {
+            cookie(cookie)
+        } When {
+//            param("createDto", createDto)
+            post("/exercises/")
+        } Then {
+            val body = Jsoup.parse(extract().body().asString())
+            Assertions.assertThatSpec(body) {
+                node("#exercisesTable") { exists() }
+                node("#exercises-list") { exists() }
+                node("td") { exists() }
+                node("#pagination") { exists() }
             }
         }
     }
